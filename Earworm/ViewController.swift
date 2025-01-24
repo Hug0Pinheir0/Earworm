a8e2644344eaddc27fcbef4155542a2b5a5303a2
Hugo Pinheiro <hugo_hp10@hotmail.com>
2025-01-24 11:33:24 -0300

    Initial Commit

---
+++ Earworm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Earworm
+//
+//  Created by Hugo Pinheiro  on 24/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
