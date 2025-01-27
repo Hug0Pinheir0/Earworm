796ed6107f1a77b63db11d045032aadb574b18a7
Hug0Pinheir0 <hugo_pinheiro04@icloud.com>
2025-01-27 20:08:34 -0300

    refactor(utils): move reusable functions to RSSParserUtils for better modularity and reuse

---
+++ Earworm/Utils/RSSParserUtils.swift
@@ -0,0 +1,54 @@
+//
+//  RSSParserUtils.swift
+//  Earworm
+//
+//  Created by Hugo Pinheiro  on 27/01/25.
+//
+
+import Foundation
+import UIKit
+
+struct RSSParserUtils {
+    static func cleanDescription(_ description: String) -> String {
+        let patternsToRemove = [
+            "Visit megaphone.fm/adchoices",
+            "Learn more about your ad choices."
+        ]
+        var cleanedDescription = description
+        for pattern in patternsToRemove {
+            cleanedDescription = cleanedDescription.replacingOccurrences(of: pattern, with: "")
+        }
+        if let plainText = convertHTMLToPlainText(html: cleanedDescription) {
+            cleanedDescription = plainText
+        }
+        return cleanedDescription.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+
+    static func convertHTMLToPlainText(html: String) -> String? {
+        guard let data = html.data(using: .utf8) else { return nil }
+        do {
+            let attributedString = try NSAttributedString(
+                data: data,
+                options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue],
+                documentAttributes: nil
+            )
+            return attributedString.string
+        } catch {
+            print("Erro ao converter HTML para texto: \(error)")
+            return nil
+        }
+    }
+
+    static func formatDuration(_ duration: String) -> String {
+        guard let totalSeconds = Int(duration) else { return "00:00:00" }
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let seconds = totalSeconds % 60
+        if hours > 0 {
+            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            return String(format: "%02d:%02d", minutes, seconds)
+        }
+    }
+}
+
